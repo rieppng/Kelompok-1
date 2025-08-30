4ed1921540fb289fea6899fdf3a991d88d5fce7b
Aulia Arief <ariefauliapku@gmail.com>
2025-08-30 21:36:00 +0700

    Add files via upload

---
+++ codePraktikum1/Praktikum1.java
@@ -0,0 +1,9 @@
+package com.mycompany.praktikum1;
+
+public class Praktikum1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama saya Aulia Arief");
+        System.out.println("Nim saya 2407112796");
+    }
+}
