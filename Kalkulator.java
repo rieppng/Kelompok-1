d3fdb12f4a9cf127455fd74971101d917d243075
BlueBird <rajavarelanandaputra7206@gmail.com>
2025-09-17 00:32:15 +0700

    Add files via upload

---
+++ Kalkulator.java
@@ -0,0 +1,15 @@
+package com.mycompany.praktikum_5;
+
+/**
+ *
+ * @author Varel
+ */
+public class Kalkulator {
+    public int tambah (int a, int b){
+        return a + b;
+    }
+    
+    public double tambah (double a, double b){
+        return a + b;
+    }
+}
---
+++ Tugas_Praktikum_5.java
@@ -0,0 +1,22 @@
+package com.mycompany.praktikum_5;
+
+/**
+ *
+ * @author Varel
+ */
+public class Tugas_Praktikum_5 {
+    public static void main(String[] args) {
+        Mahasiswa m1 = new Mahasiswa();                  
+        Mahasiswa m2 = new Mahasiswa("Varel");             
+        Mahasiswa m3 = new Mahasiswa("Jepri", 20, "2407113635");
+        
+        m1.tampilkanInpo();
+        m2.tampilkanInpo();
+        m3.tampilkanInpo();
+
+
+        m1.tampilkanInpo("Aurel");
+        m1.tampilkanInpo("Widya", 20);
+        m1.tampilkanInpo("Puput", 19, "2407113876");
+    }
+}
