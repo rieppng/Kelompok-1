93e4fd87eb5da5b79bd8cf609bf42dd2c1f4ad01
BlueBird <rajavarelanandaputra7206@gmail.com>
2025-10-26 16:02:47 +0700

    Add files via upload

---
+++ codePraktikum8/Praktikum_8.java
@@ -0,0 +1,20 @@
+package com.mycompany.praktikum_8;
+
+import javax.swing.*;
+
+public class Praktikum_8 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Contoh JFrame");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+
+        JLabel label = new JLabel("Contoh Label", SwingConstants.CENTER);
+        frame.add(label);
+        JTextField textField = new JTextField(20);;
+        frame.add(textField);
+        JButton button = new JButton("Click me");
+        frame.add(button);
+    }
+}
